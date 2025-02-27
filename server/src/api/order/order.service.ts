654533806ddb01254bc0692c274152c73e6eb7c4
HasanaghaAzad <hasanaghaazad@gmail.com>
2025-02-27 17:16:23 +0200

    generateUniqueId in backend

--- server/src/api/order/order.service.ts
+++ server/src/api/order/order.service.ts
@@ -11,7 +11,24 @@ export class OrderService {
   ) {}
 
   async create(order: Order): Promise<Order> {
+    order.uniqueId = await this.generateUniqueId();
     return this.orderRepository.save(order);
+  }
+
+  async generateUniqueId(): Promise<string> {
+    const generateId = () => {
+      const chars = 'ABCDEFGHJKLMNPQRSTWXYZ123456789'; // without confusing character
+      return Array.from(
+        { length: 8 },
+        () => chars[Math.floor(Math.random() * chars.length)],
+      ).join('');
+    };
+
+    const uniqueId: string = generateId();
+    const exists = await this.findOne(uniqueId);
+
+    if (exists) return this.generateUniqueId();
+    return uniqueId;
   }
 
   async findAll(): Promise<Order[]> {
