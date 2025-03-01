41d799448050a525952c1a59ff0d8dcd6a453273
HasanaghaAzad <hasanaghaazad@gmail.com>
2025-03-01 11:36:41 +0200

    cleaned code files related routing and menu

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { OrdersComponent } from './pages/orders/orders.component';
 import { CreateOrderComponent } from './pages/create-order/create-order.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: OrdersComponent },
   { path: 'create-order', component: CreateOrderComponent },
 ];
--- client/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { OrdersComponent } from './pages/orders/orders.component';
-import { CreateOrderComponent } from './pages/create-order/create-order.component';
-
-export const routes: Routes = [
-  { path: '', component: OrdersComponent },
-  { path: 'create-order', component: CreateOrderComponent }
-];
