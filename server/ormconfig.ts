4843450b60837725d620f8ddd51eae269831757f
HasanaghaAzad <hasanaghaazad@gmail.com>
2025-03-01 11:50:55 +0200

    tiny improve in ormconfig

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -3,7 +3,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 const config: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432', 10),
+  port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
