cb15665ccee5dc3be0ef389f2927e5afaebcfa1e
HasanaghaAzad <hasanaghaazad@gmail.com>
2025-03-01 11:32:28 +0200

    cleaning

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { OrdersComponent } from './pages/orders/orders.component';
 import { CreateOrderComponent } from './pages/create-order/create-order.component';
 
 const routes: Routes = [
-  { path: '', component: OrdersComponent },  // Empty path is default route
-  { path: 'create-order', component: CreateOrderComponent }
+  { path: '', component: OrdersComponent },
+  { path: 'create-order', component: CreateOrderComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)], // Ensure forRoot is used
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
