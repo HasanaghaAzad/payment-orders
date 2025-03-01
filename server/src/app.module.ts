c4b910017071407712b912f2895c123f0051e889
HasanaghaAzad <hasanaghaazad@gmail.com>
2025-03-01 11:45:50 +0200

    more cleaning related to app service and module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import ormconfig from '../ormconfig';
 import { OrderModule } from './api/order/order.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(ormconfig), OrderModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
